f484a3c4839da65321a9794570d10e764d9bf0ae
SiddharthNITSURAT <shsheth2006@gmail.com>
2026-05-09 20:17:51 +0530

    added counter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Hero from "../components/Hero";
 import ProjectsSection from "../components/ProjectsSection";
 import About from "../components/About";
@@ -6,6 +7,13 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import TornOverlay from "../components/TornOverlay";
 export default function Home() {
+  useEffect(() => {
+    if (!sessionStorage.getItem("visited")) {
+      fetch("https://api.counterapi.dev/v1/siddharth251206-portfolio/visits/up")
+        .then(() => sessionStorage.setItem("visited", "true"))
+        .catch(err => console.error("Failed to increment views:", err));
+    }
+  }, []);
 
 return (<>
       <Navbar />
